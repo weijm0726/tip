62aaa36e004e63cb255fb4c4c3165ccc762b5306
Brijesh Singh <brijesh.singh@amd.com>
2017-06-29 13:15:17 -0500

    crypto: ccp - remove ccp_present() check from device initialize
    Since SP device driver supports multiples devices (e.g CCP, PSP), we
    should not fail the driver init just because CCP device is not found.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- drivers/crypto/ccp/sp-dev.c
+++ drivers/crypto/ccp/sp-dev.c
@@ -244,12 +244,6 @@ static int __init sp_mod_init(void)
 	if (ret)
 		return ret;
 
-	/* Don't leave the driver loaded if init failed */
-	if (ccp_present() != 0) {
-		sp_pci_exit();
-		return -ENODEV;
-	}
-
 	return 0;
 #endif
 
@@ -259,12 +253,6 @@ static int __init sp_mod_init(void)
 	ret = sp_platform_init();
 	if (ret)
 		return ret;
-
-	/* Don't leave the driver loaded if init failed */
-	if (ccp_present() != 0) {
-		sp_platform_exit();
-		return -ENODEV;
-	}
 
 	return 0;
 #endif
