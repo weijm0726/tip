0576fd5c74440ad0250f16ac6609ecf587812456
Tom Lendacky <thomas.lendacky@amd.com>
2017-07-17 12:57:50 -0500

    compiler-gcc.h: Introduce __nostackp function attribute
    Create a new function attribute, __nostackp, that can used to turn off
    stack protection on a per function basis.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -470,6 +470,10 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 #define __visible
 #endif
 
+#ifndef __nostackp
+#define __nostackp
+#endif
+
 /*
  * Assume alignment of return value.
  */
