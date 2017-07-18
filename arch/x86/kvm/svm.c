a06c9f1e07b1899bae0598d2bf717f520731e18b
Brijesh Singh <brijesh.singh@amd.com>
2017-07-18 09:30:35 -0500

    KVM: SVM: Add support for SEV LAUNCH_MEASURE command
    The command is used to retrieve the measurement of memory encrypted
    through the LAUNCH_UPDATE_DATA command. This measurement can be used
    for attestation purposes.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5957,6 +5957,54 @@ e_free:
 	return ret;
 }
 
+static int sev_launch_measure(struct kvm *kvm, struct kvm_sev_cmd *argp)
+{
+	struct sev_data_launch_measure *data = NULL;
+	struct kvm_sev_launch_measure params;
+	void *addr = NULL;
+	int ret;
+
+	if (!sev_guest(kvm))
+		return -ENOTTY;
+
+	if (copy_from_user(&params, (void *)argp->data,
+				sizeof(struct kvm_sev_launch_measure)))
+		return -EFAULT;
+
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	if (params.address && params.length) {
+		ret = -EFAULT;
+		addr = kzalloc(params.length, GFP_KERNEL);
+		if (!addr)
+			goto e_free;
+		data->address = __psp_pa(addr);
+		data->length = params.length;
+	}
+
+	data->handle = sev_get_handle(kvm);
+	ret = sev_issue_cmd(kvm, SEV_CMD_LAUNCH_MEASURE, data, &argp->error);
+
+	/* copy the measurement to userspace */
+	if (addr &&
+		copy_to_user((void *)params.address, addr, params.length)) {
+		ret = -EFAULT;
+		goto e_free;
+	}
+
+	params.length = data->length;
+	if (copy_to_user((void *)argp->data, &params,
+				sizeof(struct kvm_sev_launch_measure)))
+		ret = -EFAULT;
+
+e_free:
+	kfree(addr);
+	kfree(data);
+	return ret;
+}
+
 static int svm_memory_encryption_op(struct kvm *kvm, void __user *argp)
 {
 	struct kvm_sev_cmd sev_cmd;
@@ -5978,6 +6026,10 @@ static int svm_memory_encryption_op(struct kvm *kvm, void __user *argp)
 	}
 	case KVM_SEV_LAUNCH_UPDATE_DATA: {
 		r = sev_launch_update_data(kvm, &sev_cmd);
+		break;
+	}
+	case KVM_SEV_LAUNCH_MEASURE: {
+		r = sev_launch_measure(kvm, &sev_cmd);
 		break;
 	}
 	default:
